d0b278bcc3c339358dbd0e62d29dcdf31761a221
Paul Holden <paul.holden@clio.com>
2017-08-19 12:30:15 -0700

    Actually the last verse. More hacky?

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -5,7 +5,9 @@ class Bottles
     if (n > 2)
       result += default_second_verse(n-1)
     elsif (n == 2)
-      result += second_last_verse
+      result += third_last_verse
+    elsif (n == 1)
+      result = second_last_verse
     else
       result = last_verse
     end
@@ -18,13 +20,18 @@ class Bottles
     "Take one down and pass it around, #{m} bottles of beer on the wall.\n"
   end
 
-  def second_last_verse
+  def third_last_verse
     "Take one down and pass it around, 1 bottle of beer on the wall.\n"
   end
 
-  def last_verse
+  def second_last_verse
     "1 bottle of beer on the wall, 1 bottle of beer.\n" +
     "Take it down and pass it around, no more bottles of beer on the wall.\n"
+  end
+
+  def last_verse
+    "No more bottles of beer on the wall, no more bottles of beer.\n" +
+    "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
   end
 
 end
