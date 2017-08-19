54d675f4b67d55be98e73992b4bde12d3f0f9d83
Paul Holden <paul.holden@clio.com>
2017-08-19 12:22:26 -0700

    Works for 2 bottles

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,13 +1,23 @@
 class Bottles
 
   def verse(n)
-    "#{n} bottles of beer on the wall, #{n} bottles of beer.\n" + default_second_verse(n-1)
+    result = "#{n} bottles of beer on the wall, #{n} bottles of beer.\n"
+    if (n > 2)
+      result += default_second_verse(n-1)
+    else
+      result += second_last_verse
+    end
+    result
   end
 
   private
 
   def default_second_verse(m)
     "Take one down and pass it around, #{m} bottles of beer on the wall.\n"
+  end
+
+  def second_last_verse
+    "Take one down and pass it around, 1 bottle of beer on the wall.\n"
   end
 
 end
